d934e1422456a60fdc9bc97769c2e321f22832b9
DanielMcGeough <daniel.mcgeough@altasschool.com>
2024-06-01 23:17:09 +0000

    alterations to BaseModel without filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-"""This defines the class called BaseModel"""
+
 import models
-from uuid import uuid4
-from datetime import datetime
+import datetime
+import uuid
 
+class BaseModel():
 
-class BaseModel:
-    """Represents BaseModel class for AirBnB project"""
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel instance
-        Args:
-            *args - a tuple
-            **kwargs - a dictionary
-        """
-        timeform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        
+
+        if (kwargs):
+            for argkey, argval in kwargs.items():
+                if argkey != '__class__':
+                    if argkey == 'created_at' or argkey == 'updated_at':
+                        argval = datetime.datetime.fromisoformat(argval)
+                    setattr(self, argkey, argval)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        return self.__dict__
