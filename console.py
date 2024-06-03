79b061bc38a4983b31e7d00b822af13962e7f4fd
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-03 23:46:08 +0000

    added the create command to console.

--- console.py
+++ console.py
@@ -82,6 +82,22 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
+
     def do_EOF(self, arg):
         """EOF signal to exit the program"""
         return True
+
+    def do_create(self, arg):
+        """
+        Creates a new class instance
+        and prints its id.
+        """
+        arg1 = parse(arg)
+        if len(arg1) == 0:
+            print("** class name missing **")
+        elif arg1[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg1[0])().id)
+            storage.save()
+        
