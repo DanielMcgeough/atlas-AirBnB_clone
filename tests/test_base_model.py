f8ad3760b448171dd0ed88eac0e77fedb9597f3d
BradBrown6621 <6621@holbertonstudents.com>
2024-06-04 21:20:43 -0500

    Attempting test for 'save'

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 
+import datetime
 import unittest
 import models
-
+from unittest.mock import MagicMock
+from models.engine.file_storage import FileStorage
 
 class TestBaseModel(unittest.TestCase):
 
@@ -14,7 +16,12 @@ class TestBaseModel(unittest.TestCase):
         del self.b1
 
     def test_save(self):
-        self.assertTrue(hasattr(self.b1, 'save'))
+        dummy_updated_at = self.b1.updated_at
+
+        self.b1.save()
+
+        self.assertNotEqual(self.b1.updated_at, dummy_updated_at)
+        self.assertIsInstance(self.b1.updated_at, datetime.datetime)
 
     def test_to_dict(self):
         self.assertTrue(isinstance(self.b1.to_dict(), dict))
